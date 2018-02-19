939bb880b5c6951778ee6484d693555ec00e2593
Janne Alatalo <git@janne-alatalo.fi>
2018-02-19 22:49:43 +0200

    Add super hacky code of discovery filter setting
    Pls remove the super hacky code at some point.

--- src/dbus_bluez.rs
+++ src/dbus_bluez.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::error;
+use std::collections::HashMap;
 use dbus;
 use device;
 
@@ -7,6 +8,7 @@ static BLUEZ_SERVICE: &'static str = "org.bluez";
 static BLUEZ_INTERFACE_ADAPTER1: &'static str = "org.bluez.Adapter1";
 static BLUEZ_OBJECT_PATH: &'static str = "/org/bluez/hci0";
 static BLUEZ_START_DISCOVERY: &'static str = "StartDiscovery";
+static BLUEZ_SET_DISCOVERY_FILTER: &'static str = "SetDiscoveryFilter";
 
 pub struct DbusBluez {
     conn: dbus::Connection,
@@ -37,11 +39,47 @@ impl DbusBluez {
             };
             if !is_powered {
                 return Err(Box::new(io::Error::new(
-                            io::ErrorKind::Other,"The bluetooth interface is not powered on!")))
+                            io::ErrorKind::Other, "The bluetooth interface is not powered on!")))
             } else {
                 info!("Interface on");
             }
         }
+
+        let empty: Vec<dbus::MessageItem> = Vec::new();
+        let signature = dbus::Signature::new("a(s)")?;
+        let a = match dbus::MessageItemArray::new(empty, signature) {
+            Ok(a) => a,
+            Err(e) => match e {
+                _ => return Err(Box::new(io::Error::new(
+                        io::ErrorKind::Other, "dbus::ArrayError"))),
+            },
+        };
+        let filter = dbus::MessageItem::DictEntry(
+            Box::new(dbus::MessageItem::Str(String::from("UUIDs"))),
+            Box::new(dbus::MessageItem::Variant(Box::new(dbus::MessageItem::Array(a)))),
+            );
+        //let filter = dbus::MessageItem::DictEntry(
+        //    Box::new(dbus::MessageItem::Str(String::from("Transport"))),
+        //    Box::new(dbus::MessageItem::Variant(Box::new(dbus::MessageItem::Str(String::from("le"))))),
+        //    );
+
+        let signature2 = dbus::Signature::new("a{sv}")?;
+        let a2 = match dbus::MessageItemArray::new(vec!(filter), signature2) {
+            Ok(a) => a,
+            Err(e) => match e {
+                _ => return Err(Box::new(io::Error::new(
+                        io::ErrorKind::Other, "dbus::ArrayError"))),
+            },
+        };
+
+        let param = dbus::MessageItem::Array(a2);
+
+
+        let msg1 = dbus::Message::new_method_call(
+            BLUEZ_SERVICE, BLUEZ_OBJECT_PATH,
+            BLUEZ_INTERFACE_ADAPTER1, BLUEZ_SET_DISCOVERY_FILTER)?.append1(param);
+        self.conn.send_with_reply_and_block(msg1, 1000)?;
+
         let msg = dbus::Message::new_method_call(
             BLUEZ_SERVICE, BLUEZ_OBJECT_PATH, BLUEZ_INTERFACE_ADAPTER1, BLUEZ_START_DISCOVERY)?;
         self.conn.send_with_reply_and_block(msg, 1000)?;
