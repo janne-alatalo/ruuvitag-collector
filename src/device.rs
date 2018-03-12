7cdd8493f423a0f1992e32cda153ba630b093e0e
Janne Alatalo <git@janne-alatalo.fi>
2018-03-12 20:23:19 +0200

    Implement the device struct properly

--- src/device.rs
+++ src/device.rs
@@ -1,4 +1,26 @@
+use std::collections::HashMap;
 
+#[derive(Default, Debug)]
 pub struct Device {
-    uuid: String,
+    object_path: String,
+    address: Option<String>,
+    mfr_data: Option<HashMap<u16, Vec<u8>>>,
+}
+
+impl Device {
+
+    pub fn new(object_path: String) -> Device {
+        Device{object_path, ..Default::default()}
+    }
+
+    pub fn set_address(&mut self, address: String) -> &mut Self {
+        self.address = Some(address);
+        self
+    }
+
+    pub fn set_mfr_data(&mut self, mfr_data: HashMap<u16, Vec<u8>>) -> &mut Self {
+        self.mfr_data = Some(mfr_data);
+        self
+    }
+
 }
