26bc837c38f6ae099a2ac09dc82edb24fe8b3d2c
Janne Alatalo <git@janne-alatalo.fi>
2018-03-12 20:23:44 +0200

    Refactor get_managed_devices to return device vector
    Also implement the get_managed_devices to read the manufacturer data in
    GetManagedObjects dbus call.

--- src/dbus_bluez.rs
+++ src/dbus_bluez.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::io::{Error, ErrorKind};
 use std::error;
 use std::{thread, time};
 use std::collections::HashMap;
@@ -20,6 +21,10 @@ static BLUEZ_SET_DISCOVERY_FILTER: &'static str = "SetDiscoveryFilter";
 
 pub struct DbusBluez {
     conn: Connection,
+}
+
+fn new_err(msg: &str) -> Box<Error> {
+    return Box::new(Error::new(ErrorKind::Other, msg))
 }
 
 impl DbusBluez {
@@ -130,11 +135,55 @@ impl DbusBluez {
 
     }
 
-    pub fn get_managed_devices(&self) -> Result<Vec<String>, BoxErr> {
+    fn read_manufacturer_data(&self, dbusmap: &MessageItem) -> Result<HashMap<u16, Vec<u8>>, BoxErr> {
+
+        let mut map = HashMap::new();
+
+        let map_arr: &[MessageItem] = match dbusmap.inner() {
+            Ok(v) => v,
+            Err(_) => return Err(new_err("inner() is not &[MessageItem]")),
+        };
+
+        for entry in map_arr {
+            let (key_item, val_item) = match entry.inner() {
+                Ok(v) => v,
+                Err(_) => return Err(new_err("inner() is not tuple")),
+            };
+            let key: u16 = match key_item.inner() {
+                Ok(v) => v,
+                Err(_) => return Err(new_err("inner() is not u16")),
+            };
+            let variant = match *val_item {
+                MessageItem::Variant(ref v) => v,
+                _ => return Err(new_err("Not a Variant")),
+            };
+            let val: &[MessageItem] = match variant.inner() {
+                Ok(r) => r,
+                Err(_) => {
+                    return Err(new_err("inner() is not &[MessageItem]"))
+                },
+            };
+            let mut byte_arr = Vec::new();
+            for entry in val {
+                let byte: u8 = match entry.inner() {
+                    Ok(v) => v,
+                    Err(_) => return Err(new_err("Not an u8")),
+                };
+                byte_arr.push(byte);
+            }
+            map.insert(key, byte_arr);
+        }
+
+        Ok(map)
+
+    }
+
+    pub fn get_managed_devices(&self) -> Result<Vec<device::Device>, BoxErr> {
+
         let msg = Message::new_method_call(BLUEZ_SERVICE, "/",
                                              "org.freedesktop.DBus.ObjectManager",
                                              "GetManagedObjects")?;
-        let mut devices: Vec<String> = Vec::new();
+        let mut devices: Vec<device::Device> = Vec::new();
         // Similar implementation as here:
         // https://github.com/szeged/blurz/blob/7729c462439fb692f12e385a84ab371423eb4cd6/src/bluetooth_utils.rs#L53
         let result = self.conn.send_with_reply_and_block(msg, 3000)?;
@@ -144,15 +193,43 @@ impl DbusBluez {
             let (path, ifs) = i.inner().unwrap();
             let interfaces: &[MessageItem] = ifs.inner().unwrap();
             for intf in interfaces {
-                let (intf_tmp, _) = intf.inner().unwrap();
+                let (intf_tmp, prop_map) = intf.inner().unwrap();
                 let intf_str: &str = intf_tmp.inner().unwrap();
                 if intf_str == "org.bluez.Device1" {
                     let path_str: &str = path.inner().unwrap();
-                    devices.push(path_str.to_string());
+                    let mut device = device::Device::new(path_str.to_string());
+                    let prop_arr: &[MessageItem] = prop_map.inner().unwrap();
+                    for prop in prop_arr {
+                        let (key, val) = match *prop {
+                            MessageItem::DictEntry(ref k, ref v) => (k, v),
+                            _ => panic!("Unexpected type!"),
+                        };
+                        let key_str: &str = key.inner().unwrap();
+                        match key_str {
+                            "Address" => {
+                                let key_val: &str = match **val {
+                                    MessageItem::Variant(ref v) => v.inner().unwrap(),
+                                    _ => panic!("Expected Variant"),
+                                };
+                                info!("{:?}", key_val);
+                                device.set_address(key_val.to_string());
+                            },
+                            "ManufacturerData" => {
+                                let mfr_data = match **val {
+                                    MessageItem::Variant(ref v) => self.read_manufacturer_data(v)?,
+                                    _ => panic!("Expected Variant"),
+                                };
+                                device.set_mfr_data(mfr_data);
+                            },
+                            _ => continue,
+                        }
+                    }
+                    devices.push(device);
                 }
             }
         }
         Ok(devices)
+
     }
 
     fn read_manufacturer_field(&self, obj_path: &str)-> Result<MaybeMap, BoxErr> {
@@ -197,12 +274,8 @@ impl DbusBluez {
 
     pub fn get_devices(&self) -> Result<Vec<device::Device>, BoxErr> {
         let devices = self.get_managed_devices()?;
-        info!("Found devices {:?}", devices);
         for d in devices {
-            match self.read_manufacturer_field(&d)? {
-                Some(mfr_data) => info!("object {}, manufacturer data {:?}", d, mfr_data),
-                None => {},
-            };
+            info!("{:?}", d);
         }
         Ok(Vec::new())
     }
